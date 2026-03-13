ea8a94badb930089ac27009c218274e45ca3c4d6
biblely0217-crypto <biblely0217@gmail.com>
2026-03-13 09:28:16 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main()
 {
-
+    printf("%s %s %s\n", "name", "id", "value");
+    printf("%s %s %s\n", "kim", "12, 4.12);
+    printf("%s %s %s\n", "lee", 1922, 3.21);
+    printf("%s %s %s\n", "park", 432, 1.0);
 
     return 0;
 }
