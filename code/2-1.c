35fab0567711aec4f6ecffbf3a44b925f51f5b80
biblely0217-crypto <biblely0217@gmail.com>
2026-03-13 09:32:04 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-
+    printf("cahar: %u\n", sizeof(char));
+    printf("short: %u\n", sizeof(short));
+    printf("int: %u\n", sizeof(int));
+    printf("unsigned int: %u\n", sizeof(unsigned int));
+    printf("long: %u\n", sizeof(long));
+    printf("long long: %u\n", sizeof(long ong));
+    printf("float: %u\n", sizeof(float));
+    
     return 0;
 }
