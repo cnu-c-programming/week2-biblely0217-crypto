a1c584baf2a51728096540d0e2ff098e542c5fbf
bible <biblely0217@gmail.com>
2026-03-20 00:04:55 +0900

    solve 3-1

--- code/3-1.c
+++ code/3-1.c
@@ -2,11 +2,10 @@
 
 int main()
 {
-     char a = 'a';
+    char a = 'a';
     char b = 'b';
 
-    printf("sizeof(a + b) is : &u\n", sizeof(a + b));
+    printf("sizeof(a + b) is : %lu\n", sizeof(a + b));
 
     return 0;
 }
-
