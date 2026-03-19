7e215b4b43ec04e23f3265fae33f8f0028f12dc5
bible <biblely0217@gmail.com>
2026-03-20 00:39:28 +0900

    solve 1-2

--- code/1-2.c
+++ code/1-2.c
@@ -3,9 +3,9 @@
 int main()
 {
     printf("name        id        value\n");
-    printf("%s %010d %10.2f\n", "kim", 12, 4.12);
-    printf("%s %010d %10.2f\n", "lee", 1922, 3.21);
-    printf("%s %010d %10.2f\n", "park", 432, 1.0);
+    printf("%s  %010d %10.2f\n", "kim", 12, 4.12);
+    printf("%s  %010d %10.2f\n", "lee", 1922, 3.21);
+    printf("%s  %010d %10.2f\n", "park", 432, 1.0);
 
     return 0;
 }
