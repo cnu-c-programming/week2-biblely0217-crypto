a23a842085653c252e59c4c9e4c550e11c3e7f69
bible <biblely0217@gmail.com>
2026-03-20 00:01:17 +0900

    solve 2-1

--- code/2-1.c
+++ code/2-1.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    printf("cahar: %u\n", sizeof(char));
-    printf("short: %u\n", sizeof(short));
-    printf("int: %u\n", sizeof(int));
-    printf("unsigned int: %u\n", sizeof(unsigned int));
-    printf("long: %u\n", sizeof(long));
-    printf("long long: %u\n", sizeof(long ong));
-    printf("float: %u\n", sizeof(float));
-    
+    printf("char: %lu\n", sizeof(char));
+    printf("short: %lu\n", sizeof(short));
+    printf("int: %lu\n", sizeof(int));
+    printf("unsigned int: %lu\n", sizeof(unsigned int));
+    printf("long: %lu\n", sizeof(long));
+    printf("long long: %lu\n", sizeof(long long));
+    printf("float: %lu\n", sizeof(float));
+
     return 0;
 }
